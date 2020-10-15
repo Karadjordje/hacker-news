075faae4cee6ad78162c21219b5d5a978a15dc4d
Karadjordje <sveljod@gmail.com>
2020-10-15 15:14:34 +0200

    Updates

---
+++ precache-manifest.394e268372c6a40b6037374f551da273.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cff1f04ee71716b1bbebf11e18821f5",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "b379256f72f96a2e877b",
+    "url": "/hacker-news/static/css/main.225d3504.chunk.css"
+  },
+  {
+    "revision": "3756217c9d04b8d58744",
+    "url": "/hacker-news/static/js/2.30d83565.chunk.js"
+  },
+  {
+    "revision": "7fca960e143ccca53aa4bc488df07e9a",
+    "url": "/hacker-news/static/js/2.30d83565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b379256f72f96a2e877b",
+    "url": "/hacker-news/static/js/main.95825d40.chunk.js"
+  },
+  {
+    "revision": "f934003405a453338133",
+    "url": "/hacker-news/static/js/runtime-main.bdc9e62f.js"
+  },
+  {
+    "revision": "c4a4aa42ae727e19a09fd5ce6db2e774",
+    "url": "/hacker-news/static/media/refresh.c4a4aa42.svg"
+  }
+]);
